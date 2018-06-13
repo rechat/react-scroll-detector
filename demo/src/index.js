62355b35e9e2c6aa3cf8bfb302adee9cbb4d80bf
Ramin <ramin.mousavy@gmail.com>
2018-06-13 14:25:43 +0430

    implement demo page

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,74 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-import Example from '../../src'
+import ReactScrollDetector from '../../src'
+import '../styles/main.css'
 
 class Demo extends Component {
+  state = {
+    log: [],
+    accuracy: 90
+  }
+
+  handleScrollBottom = () => this.addLog('Scrolled to Bottom !')
+  handleScrollTop = () => this.addLog('Scrolled to Top !')
+
+  addLog = text => {
+    const { log } = this.state
+
+    this.setState({
+      log: [text, ...log]
+    })
+  }
+
   render() {
-    return <div>
-      <h1>react-scroll-detector Demo</h1>
-      <Example/>
-    </div>
+    const { accuracy } = this.state
+
+    return (
+      <div>
+        <h1>
+          react-scroll-detector Demo <span>👍</span>
+        </h1>
+
+        <div className="row">
+          <div className="log">
+            <p>Log:</p>
+            {this.state.log.map((log, index) => <div key={index}>{log}</div>)}
+          </div>
+
+          <div className="config">
+            <div>
+              Accuracy:{' '}
+              <input
+                type="range"
+                min="0"
+                max="100"
+                value={accuracy}
+                onChange={e => this.setState({ accuracy: e.target.value })}
+              />
+              {accuracy}px
+            </div>
+
+            <div>Debounce: 500ms</div>
+          </div>
+        </div>
+
+        <ReactScrollDetector
+          accuracy={accuracy}
+          onScrollBottom={this.handleScrollBottom}
+          onScrollTop={this.handleScrollTop}
+        >
+          <div className="container">
+            {Array.from({ length: 30 }, (_, index) => (
+              <div key={index} className="item">
+                Item {index + 1}
+              </div>
+            ))}
+          </div>
+        </ReactScrollDetector>
+      </div>
+    )
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'))
