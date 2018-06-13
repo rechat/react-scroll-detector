ad30e9adcc3280d44621581772945155856ee40a
Ramin <ramin.mousavy@gmail.com>
2018-06-13 14:25:30 +0430

    implement scroll detector

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
-import React, {Component} from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
+import debounce from 'debounce'
 
-export default class extends Component {
+export default class ReactScrollDetector extends React.Component {
+  static propTypes = {
+    onScrollTop: PropTypes.func,
+    onScrollBottom: PropTypes.func,
+    accuracy: PropTypes.number,
+    debounceTime: PropTypes.number
+  }
+
+  static defaultProps = {
+    onScrollTop: () => {},
+    onScrollBottom: () => {},
+    accuracy: 90,
+    debounceTime: 500
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.onScroll = debounce(this.onScroll, this.props.accuracy)
+  }
+
+  onScroll(el) {
+    const { accuracy, onScrollBottom, onScrollTop } = this.props
+    const top = el.scrollTop - el.clientTop
+    const end = el.scrollHeight - el.offsetHeight
+
+    if (top >= end - accuracy) {
+      this.lastScrollTop = top
+
+      return onScrollBottom(top)
+    }
+
+    const isScrolledToTop = top < this.lastScrollTop
+
+    this.lastScrollTop = top
+
+    if (isScrolledToTop && top <= accuracy) {
+      onScrollTop(top)
+    }
+  }
+
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return (
+      <div onScroll={e => this.onScroll(e.target)}>{this.props.children}</div>
+    )
   }
 }
